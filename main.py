4a066c7d7fbbd1be4725f730af8dec069eb572e0
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2026-01-22 16:05:46 -0500

    Bioteck

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, render_template_string, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/api/time')
+def get_time():
+    now = datetime.now()
+    time_string = now.strftime('%I:%M %p')
+    return jsonify({'time': time_string})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)

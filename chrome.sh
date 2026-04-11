76d83a34436301a3f6f9903226d45015fdd182dd
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 14:08:53 -0400

    yes

---
+++ chrome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 2
+chromium --kiosk --password-store=basic http://127.0.0.1:5001 &
--- run.sh
+++ run.sh
@@ -3,4 +3,3 @@
 cd /home/shaun/Bioteck
 
 python3 main.py &
-chromium --kiosk --incognito --password-store=basic http://127.0.0.1:5001 &

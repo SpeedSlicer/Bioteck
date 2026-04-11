08fcad229fa111f59599781ac3bb8103463c8dc2
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 15:12:56 -0400

    asa

--- run.sh
+++ run.sh
@@ -2,14 +2,22 @@
 
 cd /home/shaun/Bioteck
 
+export DISPLAY=:0
+export XAUTHORITY=/home/shaun/.Xauthority
+
+# kill old Flask if it exists (prevents port conflict)
 pkill -f main.py
 
-export DISPLAY=:0
-
+# start Flask
 python3 main.py > flask.log 2>&1 &
 
+# wait for server
 until curl -s http://127.0.0.1:5001 > /dev/null; do
   sleep 0.5
 done
 
-chromium --kiosk --noerrdialogs --disable-infobars http://127.0.0.1:5001
+# start Chromium in REAL GUI session
+chromium --kiosk \
+  --noerrdialogs \
+  --disable-infobars \
+  http://127.0.0.1:5001

c5ed24e19eb50a575b028401163dff13927f8fa3
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 14:57:15 -0400

    asda

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, render_template_string, jsonify
+from flask import Flask, render_template, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
@@ -10,8 +10,7 @@ def index():
 @app.route('/api/time')
 def get_time():
     now = datetime.now()
-    time_string = now.strftime('%I:%M %p')
-    return jsonify({'time': time_string})
+    return jsonify({'time': now.strftime('%I:%M %p')})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=False)
--- run.sh
+++ run.sh
@@ -2,4 +2,14 @@
 
 cd /home/shaun/Bioteck
 
-python main.py &
+pkill -f main.py
+
+export DISPLAY=:0
+
+python3 main.py > flask.log 2>&1 &
+
+until curl -s http://127.0.0.1:5001 > /dev/null; do
+  sleep 0.5
+done
+
+chromium --kiosk --noerrdialogs --disable-infobars http://127.0.0.1:5001

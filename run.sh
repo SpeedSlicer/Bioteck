79dc6a0a4ed95519f5a75a3dc0dd7102374449a7
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 13:55:17 -0400

    ad

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sleep 10
-
 cd /home/shaun/Bioteck
 
-nohup /home/shaun/venv/bin/python main.py > /home/shaun/app.log 2>&1 &
-
-DISPLAY=:0 nohup /usr/bin/chromium --kiosk http://127.0.0.1:5001 > /home/shaun/browser.log 2>&1 &
+python3 main.py &
+chromium --kiosk --incognito --password-store=basic http://127.0.0.1:5001 &

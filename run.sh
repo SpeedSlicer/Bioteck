19bf0bf472b8027b6600d9323aae275dbb1e1576
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 16:16:16 -0400

    ass

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # ai has no clue what its doing. im on my own
+sleep 3
 nohup python3 -m http.server 5001 > /dev/null 2>&1 &
+sleep 1
 /bin/chromium-browser  --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  https://time.is/ &

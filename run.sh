968184ded9a1a7d02e22a6a08e1553319a540ee1
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 16:14:18 -0400

    as

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # ai has no clue what its doing. im on my own
 nohup python3 -m http.server 5001 > /dev/null 2>&1 &
-xset s noblank
-xset s off
-xset -dpms
-
-unclutter -idle 0 &
-
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-chromium-browser --kiosk --noerrdialogs --disable-infobars http://127.0.0.1:5001 &
+/bin/chromium-browser  --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  https://time.is/ &

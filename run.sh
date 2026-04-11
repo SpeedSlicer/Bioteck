bbe14596040a34a388cdf583b59c84b46d421783
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 15:31:00 -0400

    j

--- run.sh
+++ run.sh
@@ -4,8 +4,17 @@ cd /home/shaun/Bioteck
 
 export DISPLAY=:0
 export XAUTHORITY=/home/shaun/.Xauthority
-lxterminal -e "/home/shaun/Bioteck/create_python_server.sh"
-sleep 2
+
+# prevent duplicates
+pkill -f main.py
+
+# open terminal AND run Flask ONCE
+lxterminal -e "bash -c 'python3 main.py'" &
+
+# wait for server
+until curl -s http://127.0.0.1:5001 > /dev/null; do
+  sleep 0.5
+done
 
 chromium --kiosk \
   --noerrdialogs \

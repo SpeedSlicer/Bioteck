f2017cf51000c1df23503f70300f89963dbd2964
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 13:19:09 -0400

    ada

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 source /home/shaun/venv/bin/activate
 cd /home/shaun/Bioteck
 
-nohup python3 main.py > app.log 2>&1 &
+nohup python main.py > app.log 2>&1 &
+SERVER_PID=$!
+
+until curl -s http://127.0.0.1:5001 > /dev/null; do
+  sleep 1
+done
+
 nohup chromium --kiosk http://127.0.0.1:5001 > browser.log 2>&1 &
+
+wait $SERVER_PID

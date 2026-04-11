825e19e9978eb358a377e2700758f6af5bd61342
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-04-11 13:14:42 -0400

    yes

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/shaun/venv
+source /home/shaun/venv/bin/activate
 cd /home/shaun/Bioteck
-python3 main.py
-chromium --kiosk http://127.0.0.1:5001
+
+nohup python3 main.py > app.log 2>&1 &
+nohup chromium --kiosk http://127.0.0.1:5001 > browser.log 2>&1 &
